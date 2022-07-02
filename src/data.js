2e97d49526cbbb6ab928d1013467dd1a8b2a4f20
Snehal Karki <57265358+Ksnehal073@users.noreply.github.com>
2022-07-03 00:15:24 +0530

    Simple Slider using React(Naruto Pedia)

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDCtQpC87YHav4Kk0v3SgoiRy56l88frnekVyZrZocIr50yssN45NUqioOyKHEGfCkvow&usqp=CAU',
+    name: 'Madara Uchiha',
+    title: 'founder of Konohagakure',
+    quote:
+      'Madara Uchiha (うちはマダラ, Uchiha Madara) was the legendary leader of the Uchiha clan. He founded Konohagakure alongside his childhood friend and rival, Hashirama Senju, with the intention of beginning an era of peace.',
+  },
+  {
+    id: 2,
+    image:
+      'https://www.slashfilm.com/img/gallery/how-to-watch-the-naruto-franchise-in-order/l-intro-1655905173.jpg',
+    name: 'Naruto Uzumaki',
+    title: 'shinobi',
+    quote:
+      'A fate that caused him to be shunned by most of Konoha throughout his childhood..',
+  },
+  {
+    id: 3,
+    image:
+      'https://static1.cbrimages.com/wordpress/wp-content/uploads/2021/03/Sasuke-Evil-Entry-4.jpg?q=50&fit=crop&w=1600&dpr=1.5',
+    name: 'Sasuke Uchiha',
+    title: ' Konohagakure',
+    quote:
+      'After his older brother, Itachi, slaughtered their clan, Sasuke made it his mission in life to avenge them by killing Itachi.',
+  },
+  {
+    id: 4,
+    image:
+      'https://wallpapers.com/images/high/face-close-up-itachi-uchiha-8rnu7j2hbvgd4sb6.jpg',
+    name: 'Itachi Uchiha',
+    title: 'Akatsuki',
+    quote:
+      'Following his death, Itachis motives were revealed to be more complicated than they seemed and that his actions were only ever in the interest of his brother and village, making him remain a loyal shinobi of Konohagakure to the very end.',
+  },
+];
+
+export default people;
